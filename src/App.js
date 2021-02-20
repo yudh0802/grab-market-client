2f8a99f50020520621cdfcbeedcceb325507702a
yudh0802 <netking77@naver.com>
2021-02-21 02:38:55 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import 'antd/dist/antd.css';
 import './App.css';
 import MainPageComponent from './main/index.js';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
 import ProductPageComponent from './product';
 import UploadPageComponent from './upload';
+import { Button } from 'antd';
+import { CloudDownloadOutlined, DownloadOutlined } from '@ant-design/icons';
 
 function App() {
+    const history = useHistory();
     return (
         <div>
-            <Switch>
-                <Route exact={true} path="/">
-                    <MainPageComponent />;
-                </Route>
-                <Route exact={true} path="/products/:id">
-                    <ProductPageComponent />;
-                </Route>
-                <Route exact={true} path="/upload">
-                    <UploadPageComponent />;
-                </Route>
-            </Switch>
+            <div id="header">
+                <div id="header-area">
+                    <Link to="/">
+                        <img src="/images/icons/logo.png" />
+                    </Link>
+                    <Button
+                        size="large"
+                        onClick={function () {
+                            history.push('/upload');
+                        }}
+                        icon={<DownloadOutlined />}
+                    >
+                        상품 업로드
+                    </Button>
+                </div>
+            </div>
+            <div id="body">
+                <Switch>
+                    <Route exact={true} path="/">
+                        <MainPageComponent />;
+                    </Route>
+                    <Route exact={true} path="/products/:id">
+                        <ProductPageComponent />;
+                    </Route>
+                    <Route exact={true} path="/upload">
+                        <UploadPageComponent />;
+                    </Route>
+                </Switch>
+            </div>
+            <div id="footer"></div>
         </div>
     );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,10 +1,42 @@
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './index.css';
 
 function ProductPageComponent() {
-    
     const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    useEffect(function () {
+        axios
+            .get(`https://59de359d-ce1f-4d33-9afc-ce76665e5305.mock.pstmn.io/products/${id}`)
+            .then(function (result) {
+                setProduct(result.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }, []);
+    if (product === null) {
+        return <h1>상품 정보를 받고 있습니다..</h1>;
+    }
 
-    return <h1>상품 상세 관련 {id} 상품 </h1>;
+    return (
+        <div>
+            <div id="image-box">
+                <img src={'/' + product.imageUrl} />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" />
+                <span>{product.seller}</span>
+            </div>
+            <div id="contents-box">
+                <div id="name">{product.name}</div>
+                <div id="price">{product.price}원</div>
+                <div id="createdAt">2020년 12월 8일</div>
+                <div id="description">{product.description}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ProductPageComponent;
